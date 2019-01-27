279199479c75c43557c5be4f076badd0255b8fab
atulbansal4444 <atulbansal4444@gmail.com>
2019-01-27 14:51:58 +0530

    first commit

---
+++ Auto Layout Calulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calulator
+//
+//  Created by Atul Bansal on 27/01/19.
+//  Copyright © 2019 Atul Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
